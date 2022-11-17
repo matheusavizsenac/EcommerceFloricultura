3b89e9e5635a0c0e8997e15f5fd80d2dee2d7b6c
Matheus Aviz <matheus.aviz@alunos.sc.senac.br>
2022-11-16 21:55:38 -0300

    estrutura inicial do projeto

---
+++ floricultura/floriculturaapp/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+# Create your models here.
+
+class Endereco(models.Model):
+    id = models.IntegerField(primary_key=True, auto_created=True)
+    rua = models.CharField(max_length=100, null=False)
+    bairro = models.CharField(max_length=100, null=False)
+    cidade = models.CharField(max_length=100, null=False)
+    estado = models.CharField(max_length=100, null=False)
+    numero = models.CharField(max_length=10, null=False)
+    complemento = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "{} - {}".format(self.id, self.rua)
+
+class Usuario(models.Model):
+    id = models.IntegerField(primary_key=True, auto_created=True)
+    nome = models.CharField(max_length=100, null=False)
+    senha = models.CharField(max_length=50, null=False)
+    idEndereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} - {}".format(self.id, self.nome)
+
+class Produto(models.Model):
+    id = models.IntegerField(primary_key=True, auto_created=True)
+    nome = models.CharField(max_length=100, null=False)
+    descricao = models.CharField(max_length=100)
+    preco = models.FloatField(null=False)
+    imagem = models.ImageField()
+
+    def __str__(self):
+        return "{} - {}".format(self.id, self.nome)
+
+class HistoricoCompras(models.Model):
+    id = models.IntegerField(primary_key=True, auto_created=True)
+    valor_total = models.FloatField(null=False)
+    data_compra = models.DateField(auto_now=True)
+    compras = models.ManyToManyField(Produto, related_name='historico', through='ProdutoHistoricoCompras')
+
+    def __str__(self):
+        return "{} - {}".format(self.id, self.data_compra)
+
+class ProdutoHistoricoCompras(models.Model):
+    quantidade = models.IntegerField(null=False)
+    idProduto = models.ForeignKey(Produto, on_delete=models.CASCADE, related_name="produto_historico")
+    idHistoricoCompras = models.ForeignKey(HistoricoCompras, on_delete=models.CASCADE, related_name="produto_historico")
