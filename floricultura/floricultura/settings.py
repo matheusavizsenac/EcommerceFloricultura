bfdf678d6c0d16cf19bdc7214a79ecc6005f0cff
matheusavizsenacm <matheus.aviz@alunos.sc.senac.br>
2022-11-18 19:28:02 -0300

    criando tabelas

--- floricultura/floricultura/settings.py
+++ floricultura/floricultura/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,13 +30,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'floriculturaapp'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'floriculturaapp.apps.FloriculturaappConfig'
 ]
 
 MIDDLEWARE = [
