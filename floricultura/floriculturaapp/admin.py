b43db0486132246f1dd93b02a53701aee3ed3206
matheusavizsenacm <matheus.aviz@alunos.sc.senac.br>
2022-11-18 20:01:38 -0300

    adicionando tabelas na pagina de adm

--- floricultura/floriculturaapp/admin.py
+++ floricultura/floriculturaapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from floriculturaapp.models import *
 # Register your models here.
+
+
+admin.site.register(Produto)
+admin.site.register(Usuario)
+admin.site.register(Endereco)
+admin.site.register(HistoricoCompras)
+admin.site.register(ProdutoHistoricoCompras)
