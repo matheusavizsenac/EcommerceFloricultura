e1d7990d4e4b63763b54aa83cd42c18247b256f6
matheusavizsenacm <matheus.aviz@alunos.sc.senac.br>
2022-12-07 21:14:24 -0300

    adicionando API

--- floricultura/floriculturaapp/views.py
+++ floricultura/floriculturaapp/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render
 from rest_framework.views import APIView
 from .models import Produto
@@ -43,12 +44,17 @@ class ListaDeCarrinho(APIView):
 
 class Compra(APIView):
     def post(self, request):
-        response = envia_dados_pagamento_pagseguro(request.body.decode())
+        data2 = {"reference_id":"zqp2zhf295","customer":{"name":"Matheus Aviz","email":"email@test.com","tax_id":"12345678909","phones":[{"country":"55","area":"11","number":"999999999","type":"MOBILE"}]},"items":[{"reference_id":4,"name":"Planta","quantity":1,"unit_amount":30},{"reference_id":4,"name":"Planta","quantity":1,"unit_amount":30},{"reference_id":4,"name":"Planta","quantity":1,"unit_amount":30},{"reference_id":4,"name":"Planta","quantity":1,"unit_amount":30}],"qr_code":{"amount":{"value":500}},"shipping":{"address":{"street":"General","number":"288","complement":"Casa","locality":"Velha","city":"Blumenau","region_code":"SC","country":"BRA","postal_code":"89041000"}},"notification_urls":["https://meusite.com/notificacoes"],"charges":[{"reference_id":"referencia do pagamento","description":"descricao do pagamento","amount":{"value":120,"currency":"BRL"},"payment_method":{"type":"CREDIT_CARD","installments":1,"capture":"true","card":{"number":"4111111111111111","exp_month":"12","exp_year":"2026","security_code":"123","holder":{"name":"Jose Da Silva"},"store":"false"}},"notification_urls":["https://meusite.com/notificacoes"]}]}
+        ##data1 = request.body.decode()
+        print(type(data2))
+        ##print(type(data1))
+        ##print(data1)
+        response = self.envia_dados_pagamento_pagseguro(data2)
         return Response(response)
 
-def envia_dados_pagamento_pagseguro(data):
-    response = requests.post('https://sandbox.api.pagseguro.com/orders', json=data, headers={'Authorization': 'Bearer 540CD5FAE4074E4D87F9BA20A897A93E','Content-Type': 'application/json', 'Accept': 'application/json'})
-    return response
+    def envia_dados_pagamento_pagseguro(self, data):
+        response = requests.post('https://sandbox.api.pagseguro.com/orders', json=data, headers={'Authorization': 'Bearer 540CD5FAE4074E4D87F9BA20A897A93E','Content-Type': 'application/json', 'Accept': 'application/json'})
+        print("status code: " + str(response.content))
 
 
 
